810d71be174af6a2d678b26b2c839f5737097952
Maksim59 <max.isayenka@gmail.com>
2024-05-28 20:27:24 -0400

    created a score

--- Asteroid.java
+++ Asteroid.java
@@ -53,6 +53,9 @@ public class Asteroid extends Actor
             removeTouching(Bullet.class);
             getWorld().removeObject(this);
             score++;
+            MyWorld myWorld= (MyWorld)getWorld();
+            Score scoreboard = myWorld.getScoreboard();
+            scoreboard.update(score);
         }
         
     }
--- MyWorld.java
+++ MyWorld.java
@@ -13,16 +13,18 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    private Score scoreboard;
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1, false);
-        
+
         Ship ship = new Ship();
         addObject(ship, 400,300);
         addAsteroid();
+        prepare();
     }
-    
+
     public void addAsteroid()
     {
         for(int i = 0; i < 10; i++)
@@ -30,6 +32,20 @@ public class MyWorld extends World
             addObject(new Asteroid(), Greenfoot.getRandomNumber(800), 0);
         }
     }
-    
 
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Score scoreboard = new Score(0);
+        addObject(scoreboard, getWidth()/2, scoreboard.getImage().getHeight()/2);
+        
+        
+    }
+    public Score getScoreboard()
+    {
+        return scoreboard;
+    }
 }
