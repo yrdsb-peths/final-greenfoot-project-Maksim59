4c10fbc0e396f303a5d6f6f4f40803292ebbc511
Maksim59 <max.isayenka@gmail.com>
2024-06-11 22:32:46 -0400

    Create SmallAsteroid.java

---
+++ SmallAsteroid.java
@@ -0,0 +1,74 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class smallAsteroid here.
+ * Write a description of class SmallAsteroid here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ * */
+public class SmallAsteroid extends Actor
+{
+    /**
+     * Act - do whatever the smallAsteroid wants to do. This method is called whenever
+     * Act - do whatever the SmallAsteroid wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    GreenfootSound breakSound = new GreenfootSound("sounds/rockbreak.mp3");
+    public int speedAst;
+    public SmallAsteroid(int speed)
+    {
+        setRotation(Greenfoot.getRandomNumber(360));
+        speedAst = speed;
+    }
+    public void act()
+    {
+        GreenfootImage myImage = getImage();
+        myImage.scale(25,25);
+        
+        move(-5);
+        infiniteMove();
+        colide();
+    }
+    
+    public void infiniteMove()
+    {
+        if(getX() < 0)
+        {
+            setLocation(getWorld().getWidth(), getY());
+        }
+        if(getX() > getWorld().getWidth())
+        {
+            setLocation(0, getY());
+        }
+        if(getY() < 0)
+        {
+            setLocation(getX(), getWorld().getHeight());
+        }
+        if(getY() > getWorld().getHeight())
+        {
+            setLocation(getX(), 0);
+        }
+    }
+    
+    public void colide()
+    {
+        if(isTouching(Bullet.class))
+        {
+            setLocation(getX(), 0);
+            removeTouching(Bullet.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.increaseSmallScore();
+            breakSound.play();
+            world.removeObject(this);
+            
+        }
+    }
+    
+    
+    public void reset()
+    {
+        setLocation(getX(), 0);
+        // Add your action code here.
+    }
+}
